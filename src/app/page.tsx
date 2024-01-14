2802bc39ca1c6fe647d2ac91cebba1754095576e
Jun Ukemori <jukemori@gmail.com>
2024-01-14 14:37:38 +0900

    feature: add filter fixture function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,78 @@
-import { getPlayer, getPlayerStats, getFixtures } from '@/utils';
-import { PlayerProps, StatsProps, FixtureProps } from '@/types';
-import { Stats, Fixture } from '@/components';
-import Image from 'next/image';
+import {
+  getPlayer,
+  getPlayerStats,
+  getFixtures,
+  getFixturePlayers,
+} from '@/utils'
+import { PlayerProps, StatsProps, FixtureProps } from '@/types'
+import { Stats, Fixture } from '@/components'
+import Image from 'next/image'
 
 interface HomeProps {
-  player: PlayerProps;
-  playerStatsArray: StatsProps[];
-  fixtureArray: FixtureProps[];
+  player: PlayerProps
+  playerStatsArray: StatsProps[]
+  fixtureArray: FixtureProps[]
 }
 
 async function fetchPlayerData(): Promise<HomeProps> {
-  return {
-    player: await getPlayer(),
-    playerStatsArray: await getPlayerStats(),
-    fixtureArray: await getFixtures(),
-  };
-}
+  const player = await getPlayer()
+  const playerStatsArray = await getPlayerStats()
+  const fixtureArray = await getFixtures()
 
-function sortAndFilterFixtures(fixtureArray: FixtureProps[]): FixtureProps[] {
-  const currentDate = new Date();
-  const oneWeekAgo = new Date();
-  oneWeekAgo.setDate(currentDate.getDate() - 7);
+  const currentDate = new Date()
+  const twoWeeksAgo = new Date()
+  twoWeeksAgo.setDate(currentDate.getDate() - 14)
 
   const filteredFixtures = fixtureArray
-    .filter((fixture) => {
-      const fixtureDate = new Date(fixture.fixture.date);
-      return fixtureDate >= oneWeekAgo && fixtureDate <= currentDate;
+    .filter((fixture: FixtureProps) => {
+      const fixtureDate = new Date(fixture.fixture.date)
+      return fixtureDate >= twoWeeksAgo && fixtureDate <= currentDate
     })
-    .sort((a, b) => {
-      const dateA = new Date(a.fixture.date);
-      const dateB = new Date(b.fixture.date);
-      return dateA.getTime() - dateB.getTime();
-    });
-
-  const upcomingFixtures = fixtureArray
-    .filter((fixture) => {
-      const fixtureDate = new Date(fixture.fixture.date);
-      return fixtureDate > currentDate;
+    .sort((a: FixtureProps, b: FixtureProps) => {
+      const dateA = new Date(a.fixture.date)
+      const dateB = new Date(b.fixture.date)
+      return dateA.getTime() - dateB.getTime()
     })
-    .sort((a, b) => {
-      const dateA = new Date(a.fixture.date);
-      const dateB = new Date(b.fixture.date);
-      return dateA.getTime() - dateB.getTime();
-    });
 
-  return [...filteredFixtures, ...upcomingFixtures];
+  // const upcomingFixtures = fixtureArray
+  //   .filter((fixture) => {
+  //     const fixtureDate = new Date(fixture.fixture.date)
+  //     return fixtureDate > currentDate
+  //   })
+  //   .sort((a, b) => {
+  //     const dateA = new Date(a.fixture.date)
+  //     const dateB = new Date(b.fixture.date)
+  //     return dateA.getTime() - dateB.getTime()
+  //   })
+
+  const fixturePlayersPromises = filteredFixtures.map(
+    async (fixture: FixtureProps) => {
+      const fixturePlayersArray = await getFixturePlayers(fixture)
+      return fixturePlayersArray
+    }
+  )
+
+  const fixturePlayersArray = await Promise.all(fixturePlayersPromises)
+
+  console.log(fixturePlayersArray)
+
+  const fixturesWithPlayer = filteredFixtures.filter(
+    (fixture: FixtureProps, index: number) => {
+      const hasPlayer = fixturePlayersArray[index].includes(32862)
+      return hasPlayer
+    }
+  )
+
+  console.log(fixturesWithPlayer)
+
+  return {
+    player,
+    playerStatsArray,
+    fixtureArray: fixturesWithPlayer,
+  }
 }
 
 function Home({ player, playerStatsArray, fixtureArray }: HomeProps) {
-  const filteredFixtures = sortAndFilterFixtures(fixtureArray);
-
   return (
     <div>
       <h1>Player Information:</h1>
@@ -62,15 +85,21 @@ function Home({ player, playerStatsArray, fixtureArray }: HomeProps) {
         <Stats key={index} stats={stats} />
       ))}
 
-      {filteredFixtures.map((fixture, index) => (
+      {fixtureArray.map((fixture, index) => (
         <Fixture key={index} fixture={fixture} />
       ))}
     </div>
-  );
+  )
 }
 
 export default async function HomePage() {
-  const { player, playerStatsArray, fixtureArray } = await fetchPlayerData();
+  const { player, playerStatsArray, fixtureArray } = await fetchPlayerData()
 
-  return <Home player={player} playerStatsArray={playerStatsArray} fixtureArray={fixtureArray} />;
+  return (
+    <Home
+      player={player}
+      playerStatsArray={playerStatsArray}
+      fixtureArray={fixtureArray}
+    />
+  )
 }
